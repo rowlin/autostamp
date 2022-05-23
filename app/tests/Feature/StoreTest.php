91f09a61d5aaf37a242cd8a52228f65ff808c169
alice <rowlinest90@gmail.com>
2022-05-23 06:19:05 +0300

    add feature Store test

---
+++ app/tests/Feature/StoreTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Http\Middleware\VerifyCsrfToken;
+use App\Store;
+
+class StoreTest extends TestCase
+{
+    use WithFaker;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+    }
+
+    protected function getFirstStore(){
+        return Store::first();
+    }
+
+    protected function generateNewStore(array $data = []){
+        return  [
+            'name' => $data['name'] ?? $this->faker->text(30),
+            'description' => $data['description'] ?? $this->faker->text(300),
+        ];
+    }
+
+    public function testStoreCreateNew()
+    {
+        $store = $this->generateNewStore();
+        $response = $this->post('/store/create', $store );
+        $this->assertDatabaseHas('stores' ,  $store );
+        $response->assertRedirect('stores');
+    }
+
+    public function testStoreCreateIfNameNotRequired(){
+        $store = $this->generateNewStore(['name' => '']);
+        $this->post('/store/create', $store );
+        $this->assertDatabaseMissing('stores' ,  $store );
+    }
+
+    public function testStoreUpdate(){
+        $store = $this->getFirstStore();
+        $new_store = $this->generateNewStore();
+        $this->patch('/store/update/'. $store->id , $new_store);
+        $this->assertDatabaseHas('stores' ,  $new_store);
+    }
+
+    public function testStoreDelete(){
+        $store = $this->getFirstStore();
+        $this->delete('/store/delete/'. $store->id);
+        $this->assertDatabaseMissing('stores' ,  $store->toArray());
+    }
+}
