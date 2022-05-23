5767f7eefdfd8b623214655abb9022e07543aa04
alice <rowlinest90@gmail.com>
2022-05-23 04:59:15 +0300

    add routes for store

--- app/routes/web.php
+++ app/routes/web.php
@@ -23,10 +23,10 @@ Route::post('/product/create', 'ProductController@save')->name('product.save');
 Route::patch('/product/update/{product_id}', 'ProductController@update')->where('product_id', '[0-9]+')->name('product.update');
 Route::delete('/product/delete/{product_id}' , 'ProductController@delete')->where('product_id', '[0-9]+')->name('product.delete');
 
-/*
-Route::get('/stories',  'ProductController@index')->name('product.list');
-Route::get('/store/edit/{product_id}' , 'ProductController@edit')->where('product_id', '[0-9]+')->name('product.edit');
-Route::get('/store/create', 'ProductController@create')->name('product.create');
-Route::post('/store/create', 'ProductController@save')->name('product.save');
-Route::patch('/store/update/{product_id}', 'ProductController@update')->where('product_id', '[0-9]+')->name('product.update');
-Route::delete('/store/delete/{product_id}' , 'ProductController@delete')->where('product_id', '[0-9]+')->name('product.delete');*/
+
+Route::get('/stories',  'StoreController@index')->name('stories.list');
+Route::get('/store/edit/{store_id}' , 'StoreController@edit')->where('store_id', '[0-9]+')->name('store.edit');
+Route::get('/store/create', 'StoreController@create')->name('store.create');
+Route::post('/store/create', 'StoreController@save')->name('store.save');
+Route::patch('/store/update/{store_id}', 'StoreController@update')->where('store_id', '[0-9]+')->name('store.update');
+Route::delete('/store/delete/{store_id}' , 'StoreController@delete')->where('store_id', '[0-9]+')->name('store.delete');
